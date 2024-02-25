6b1578c3408c1708a4986be93ed65e13b436d19d
hirychau <girichev@gmail.com>
2024-02-25 13:21:37 +0100

    Vars test

--- vars/slackNotify.groovy
+++ vars/slackNotify.groovy
@@ -1,8 +1,9 @@
-def call() {
+def call(MessageType type) {
     def buildNum = buildNum()
     def changes = gitChanges()
     def branch = gitBranch()
     return """
+Type: ${type.color}
 BuildNum: ${buildNum}
 Branch: ${branch}
 ${changes}
