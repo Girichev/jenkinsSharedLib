6f662ccd5004549449301e56f48d18cbf00e3961
hirychau <girichev@gmail.com>
2024-02-25 08:36:43 +0100

    Vars test

--- vars/gitChanges.groovy
+++ vars/gitChanges.groovy
@@ -1,4 +1,4 @@
-def exec() {
+def call() {
     def changes = currentBuild.changeSets.collect { cs ->
         cs.items.collect { item -> "- ${item.msg} [${item.author.fullName}]" }
     }.flatten().join("\n")
